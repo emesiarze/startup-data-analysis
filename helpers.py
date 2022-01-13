bc9bc2e0a0651d81599e9184102bb9bbb6ed41d2
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-01-13 20:33:01 +0100

    Create jupyter notebook

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def map_text_values_to_number(df, categories_dict, countries_dict):
-    categories_mapped = pd.Series(df['main_category'].map(categories_dict), name='main_category')
-    countries_mapped = pd.Series(df['country'].map(countries_dict), name='country')
-    df.update(categories_mapped)
-    df.update(countries_mapped)
-
-    return df
